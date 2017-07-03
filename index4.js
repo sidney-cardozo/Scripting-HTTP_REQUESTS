f4c726aee9e68566788db5c1e829f6d4a25e4212
Sidney Cardozo <scooby123sid@gmail.com>
2017-07-03 21:50:59 +0000

    step 4 done

---
+++ index4.js
@@ -0,0 +1,38 @@
+var https = require('https');
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    var totalData = '';
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      totalData += data;
+
+    });
+
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      callback(totalData);
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
